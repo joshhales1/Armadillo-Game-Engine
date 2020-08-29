d8752647b248173ab19acf7fa4c4372a3d9be58c
JayHales <joshhales2004@gmail.com>
2020-08-29 18:30:25 +0100

    Added Transform component.

---
+++ engine/Transform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArmadilloEngine
+{
+    public class Transform : IComponent
+    {
+        public Vector Position;
+        public Vector Rotation;
+        public GameObject Parent;
+
+        public void Execute() { }
+
+    }
+}
