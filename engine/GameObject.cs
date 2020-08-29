e87c80bcbee62676579bd7f7457c017eb87003da
JayHales <joshhales2004@gmail.com>
2020-08-29 18:30:12 +0100

    Added GameObject class.

--- engine/GameObject.cs
+++ engine/GameObject.cs
@@ -5,7 +5,21 @@ namespace ArmadilloEngine
 {
 	public class GameObject
 	{
-		public List<string> Components;
-	}
+        public List<IComponent> Components = new List<IComponent>();
+
+        public T GetComponent<T>()
+        {
+            foreach (IComponent component in Components)
+                if (component is T)
+                    return (T) component;
+            return default;
+        }
+        public void AddComponent<T>() where T : IComponent, new()
+        {
+            IComponent c = new T();
+            Components.Add(c);
+        }
+    }
+    
 }
 
