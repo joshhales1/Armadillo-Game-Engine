d1dc8ac2745e56757b8c17504f34bf15da9e4519
JayHales <joshhales2004@gmail.com>
2020-08-29 15:39:38 +0100

    Created the VS project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Armadillo_Game_Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
