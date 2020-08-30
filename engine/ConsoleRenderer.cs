c74430fcdc95e7ad7a982f49a68dc383df7e8687
JayHales <joshhales2004@gmail.com>
2020-08-30 17:57:58 +0100

    Added the first version of the console renderer

---
+++ engine/ConsoleRenderer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArmadilloEngine
+{
+    static class Renderer
+    {
+        static List<SpriteRenderer> SpritesForRender = new List<SpriteRenderer>();
+        public static Vector GameWindowDimensions = new Vector(20, 20);
+
+        static char[,] OldBuffer = new char[0, 0];
+        static char[,] CurrentBuffer = new char[0, 0];
+
+        public static void Render()
+        {
+            OldBuffer = CurrentBuffer;
+            CurrentBuffer = GenerateBuffer();
+
+            bool unmatched = false;
+            //Check if lists are same
+            if (OldBuffer.Length != CurrentBuffer.Length) unmatched = true;
+            
+            for (int x = 0; x < OldBuffer.GetLength(0); x++)
+                for (int y = 0; y < OldBuffer.GetLength(1); y++)
+                {
+                    if (OldBuffer[x, y] != CurrentBuffer[x, y])
+                    {
+                        unmatched = true;
+                        break;
+                    }
+                    if (unmatched) break;
+                }
+            if (!unmatched) return;
+
+
+            Console.Clear();
+            for (int x = 0; x < GameWindowDimensions.x; x++)
+            {
+                for (int y = 0; y < GameWindowDimensions.y; y++)
+                    Console.Write(CurrentBuffer[x, y]);
+                Console.Write("\n");
+            }
+            SpritesForRender.Clear();                
+        }
+
+        static char[,] GenerateBuffer()
+        {
+            char[,] grid = new char[(int)GameWindowDimensions.x, (int)GameWindowDimensions.y];
+
+            foreach (SpriteRenderer sprite in SpritesForRender)
+            {
+                Vector offset = sprite.Owner.GetComponent<Transform>().Position;
+                Vector dimensions = sprite.Dimensions;
+                for (int x = 0; x < dimensions.x; x++)
+                    for (int y = 0; y < dimensions.y; y++)
+                    {
+                        grid[(int)offset.x + x, (int)offset.y + y] = sprite.SpriteText[x, y];
+                    }
+                        
+            }
+            return grid;
+        }
+
+        public static void AddSprite(SpriteRenderer sprite) => SpritesForRender.Add(sprite);
+
+
+    }
+}
