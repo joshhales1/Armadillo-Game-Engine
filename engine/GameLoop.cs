ee9c65ee604a04c0493917707112326c65e832f0
JayHales <joshhales2004@gmail.com>
2020-08-30 17:58:48 +0100

    Added a game loop for automatically executing components and rendering

---
+++ engine/GameLoop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArmadilloEngine
+{
+	public static class Game
+	{
+		static List<GameObject> Objects = new List<GameObject>();
+		static bool Running;
+		static void Loop()
+        {
+			foreach (GameObject gameObject in Objects)
+            {
+				foreach (IComponent component in gameObject.Components)
+                {
+					component.Execute();
+                }
+			}
+
+			Renderer.Render();
+		}
+
+		public static void Start()
+        {
+			Running = true;
+			while (Running)
+            {
+				Loop();
+            }
+		}
+
+		public static void Stop()
+        {
+			Running = false;
+        }
+
+		public static void AddObject(GameObject gameObject)
+        {
+			Objects.Add(gameObject);
+        }
+
+	}
+
+	
+
+}
+
