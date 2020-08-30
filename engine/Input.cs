f4273411d0b5b672b5ce45cee24485fb4ddd28d8
JayHales <joshhales2004@gmail.com>
2020-08-30 22:01:16 +0100

    Added an input class

---
+++ engine/Input.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+namespace ArmadilloEngine
+{
+	public static class Input
+	{
+		public static char PressedKey;
+		public static void Start()
+        {
+			Console.WriteLine("Starting input...");
+			ThreadStart inputThreadStart = new ThreadStart(ReadInputThread);
+			Thread inputThread = new Thread(inputThreadStart);
+			inputThread.Start();
+        }
+		static void ReadInputThread()
+        {
+			while (true)
+            {
+				PressedKey = Console.ReadKey().KeyChar;
+				Renderer.RequestRender();
+			}			
+        }
+
+	}
+}
+
