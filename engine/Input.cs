8ce28e88c5d80a32b7e256f670198143ffa7f0e9
JayHales <joshhales2004@gmail.com>
2020-09-05 20:37:03 +0100

    Bug fix regarding input characters being written to the screen

--- engine/Input.cs
+++ engine/Input.cs
@@ -19,6 +19,8 @@ namespace ArmadilloEngine
             {
 				PressedKey = Console.ReadKey().KeyChar;
 				Renderer.RequestRender();
+				PressedKey = Console.ReadKey(true).KeyChar;				
+				
 			}			
         }
 
