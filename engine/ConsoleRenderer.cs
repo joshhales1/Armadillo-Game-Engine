b6a35b4e208b5def8399dd1b0243d843557d7d93
JayHales <joshhales2004@gmail.com>
2020-09-05 20:39:18 +0100

    Reimplemented the double char render mode and new render method

--- engine/ConsoleRenderer.cs
+++ engine/ConsoleRenderer.cs
@@ -6,13 +6,20 @@ namespace ArmadilloEngine
     static class Renderer
     {
         static List<SpriteRenderer> SpritesForRender = new List<SpriteRenderer>();
-        public static Vector GameWindowDimensions = new Vector(20, 20);
+        public static byte SingleChar = 1;
+        public static byte DoubleChar = 2;
 
         public static byte RenderMode = DoubleChar;
 
-        static char[,] OldBuffer = new char[0, 0];
-        static char[,] CurrentBuffer = new char[0, 0];
+        static char[,] OldBuffer = BufferTemplate();
+        static char[,] CurrentBuffer = BufferTemplate();
 
+        public static void Start()
+        {
+            Console.WriteLine(OldBuffer[10, 10]);
+            Console.CursorVisible = false;
+            SetGameWindowSize(new Vector(16, 16));
+        }
 
         public static void Render()
         {
@@ -24,61 +31,56 @@ namespace ArmadilloEngine
                 {
                     if (OldBuffer[x, y] != CurrentBuffer[x, y])
                     {
-                        if (OldBuffer[x, y] != CurrentBuffer[x, y])
+                        Console.SetCursorPosition(x * RenderMode, y);
+                        Console.Write("\b" + CurrentBuffer[x, y]);
+                        if (RenderMode == DoubleChar)
                         {
-                            unmatched = true;
-                            break;
+                            Console.SetCursorPosition(x * 2 + 1, y);
+                            Console.Write("\b" + CurrentBuffer[x, y]);
                         }
-                        if (unmatched) break;
                     }
-                if (!unmatched) return;
-            } else
-            {
-                SpecialRender = false;
-            }
-
-            Console.Clear();
-            for (int y = 0; y < GameWindowDimensions.y; y++)                
-            {
-                for (int x = 0; x < GameWindowDimensions.x; x++)
-                {
-                    Console.Write(CurrentBuffer[x, y]);
-                    if (RenderMode == DoubleChar) Console.Write(CurrentBuffer[x, y]);
                 }
-                Console.Write("\n");
-            }
-            SpritesForRender.Clear();                
-        }
-
-        public static void RequestRender()
-        {
-            SpecialRender = true;
         }
 
         static char[,] GenerateBuffer()
         {
-            char[,] grid = new char[(int)GameWindowDimensions.x, (int)GameWindowDimensions.y];
+            char[,] grid = BufferTemplate();
 
             foreach (SpriteRenderer sprite in SpritesForRender)
             {
                 Vector offset = sprite.Owner.GetComponent<Transform>().Position;
                 Vector dimensions = sprite.Dimensions;
+
                 for (int x = 0; x < dimensions.x; x++)
                     for (int y = 0; y < dimensions.y; y++)
                     {
                         int xPos = (int)offset.x + x;
                         int yPos = (int)offset.y + y;
-                        if (xPos > GameWindowDimensions.x - 1 || xPos < 0 || yPos > GameWindowDimensions.y - 1 || yPos < 0) continue;
+                        if (xPos > Console.BufferWidth / RenderMode - 1 || xPos < 0 || yPos > Console.BufferHeight - 1 || yPos < 0) continue;
                         grid[(int)offset.x + x, (int)offset.y + y] = sprite.SpriteText[x, y];
-                    }
-                        
+                    }                        
             }
+            SpritesForRender.Clear();
             return grid;
         }
 
+        public static void SetGameWindowSize(Vector a)
+        {
+            if (a.x < 16 || a.y < 16)
+            {
+                Debug.Log("Size must be larger than 15");
+                return;
+            }                
+
+            Console.SetWindowSize(1, 1);
+            Console.SetBufferSize((int)a.x * RenderMode, (int)a.y);
+            Console.SetWindowSize((int)a.x * RenderMode, (int)a.y);
+        }
+
+        static char[,] BufferTemplate() => new char[Console.BufferWidth / RenderMode, Console.BufferHeight];
+
         public static void AddSprite(SpriteRenderer sprite) => SpritesForRender.Add(sprite);
 
-        public static byte SingleChar = 0;
-        public static byte DoubleChar = 0;
+        
     }
 }
