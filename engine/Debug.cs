98be08c005040b60df9ba0d63a79cac079fbb44c
JayHales <joshhales2004@gmail.com>
2020-09-05 17:18:23 +0100

    Added basic Debug class

---
+++ engine/Debug.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+namespace ArmadilloEngine
+{
+	
+	public static class Debug
+	{
+		static bool HasStarted;
+		public static void Log(string s)
+        {
+			if (!HasStarted)
+				StartDebug();
+			File.WriteAllText("log.txt", $"{File.ReadAllText("log.txt")}[{DateTime.Now.ToString()}] {s}\n");
+		}
+
+		static void StartDebug()
+        {
+			File.WriteAllText("log.txt", "");
+			HasStarted = true;
+		}
+	}
+}
+
