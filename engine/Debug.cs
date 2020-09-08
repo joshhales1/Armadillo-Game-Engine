2d766a6aa9d1d8ae7febf71c41e42110e695a138
JayHales <joshhales2004@gmail.com>
2020-09-08 19:49:33 +0100

    Added Warn and Error methods for, well, warning and erroring

--- engine/Debug.cs
+++ engine/Debug.cs
@@ -7,12 +7,24 @@ namespace ArmadilloEngine
 	public static class Debug
 	{
 		static bool HasStarted;
+		public static string RecentMessage = "";
 		public static void Log(string s)
         {
 			if (!HasStarted)
 				StartDebug();
 			File.WriteAllText("log.txt", $"{File.ReadAllText("log.txt")}[{DateTime.Now.ToString()}] {s}\n");
+			RecentMessage = s;
 		}
+
+		public static void Warn(string s)
+        {
+			Log("[Warning] " + s);
+        }
+
+		public static void Error(string s)
+        {
+			Log("[Error]" + s);
+        }
 
 		static void StartDebug()
         {
