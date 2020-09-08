127368095f6d517675fff0485a1cac50f35d4d3f
JayHales <joshhales2004@gmail.com>
2020-09-08 20:01:33 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,20 +4,31 @@ using ArmadilloEngine;
 class Program 
 {
     static void Main()
-    {
-        
+    {        
 
         GameObject player = new GameObject();
         Transform tr = player.AddComponent<Transform>();
         tr.Position = new Vector(5, 5);
 
         SpriteRenderer sr = player.AddComponent<SpriteRenderer>();
-        sr.SetSprite("xxxxoxxxx");
+        string spriteString = "xxxxoxxxx";
+        sr.SetSprite(SpriteRenderer.StringToSpriteText(spriteString, 3, 3));
 
         player.AddComponent<ObjectController>();
-
+        
         Game.AddObject(player);
-        Game.Start();
+        Game.Start(30, 30);
+        Debug.Log("hello");
+        Debug.Log("helllllllllllllllllllllo");
+        Debug.Log("oh hi");
+        Debug.Log("helllllllllllllllllllllo");
+        Debug.Log("oh hi");
+        Debug.Log("helllllllllllllllllllllo");
+        Debug.Log("oh hi");
+        Debug.Log("helllllllllllllllllllllo");
+        Debug.Log("oh hi");
+        Debug.Log("o heyyyyyyyyyyyyyyyyyyyyyyyyy");
+
     }
 }
 
