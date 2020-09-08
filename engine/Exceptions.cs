39710a58e1bd825329d35a49826abd4dffd86b9a
JayHales <joshhales2004@gmail.com>
2020-09-08 19:50:55 +0100

    Added some custom exceptions

---
+++ engine/Exceptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArmadilloEngine
+{
+    [Serializable]
+    class ArmadilloEngineException : Exception
+    {
+        public ArmadilloEngineException()
+        {
+
+        }
+
+        public ArmadilloEngineException(string s)
+            : base(s)
+        {
+            Debug.Error(s);
+        }
+
+    }
+}
