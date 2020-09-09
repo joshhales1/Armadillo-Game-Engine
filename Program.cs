351c9c55487b4711badf4b8cec776f6565b92b93
JayHales <joshhales2004@gmail.com>
2020-09-09 18:56:12 +0100

    Cleaned whitespace

--- Program.cs
+++ Program.cs
@@ -4,8 +4,7 @@ using ArmadilloEngine;
 class Program 
 {
     static void Main()
-    {        
-
+    {
         GameObject player = new GameObject();
         Transform tr = player.AddComponent<Transform>();
         tr.Position = new Vector(5, 5);
@@ -15,20 +14,10 @@ class Program
         sr.SetSprite(SpriteRenderer.StringToSpriteText(spriteString, 3, 3));
 
         player.AddComponent<ObjectController>();
-        
-        Game.AddObject(player);
-        Game.Start(30, 30);
-        Debug.Log("hello");
-        Debug.Log("helllllllllllllllllllllo");
-        Debug.Log("oh hi");
-        Debug.Log("helllllllllllllllllllllo");
-        Debug.Log("oh hi");
-        Debug.Log("helllllllllllllllllllllo");
-        Debug.Log("oh hi");
-        Debug.Log("helllllllllllllllllllllo");
-        Debug.Log("oh hi");
-        Debug.Log("o heyyyyyyyyyyyyyyyyyyyyyyyyy");
 
+        Game.AddObject(player);
+
+        Game.Start(30, 30); // Code past this point will not execute.
     }
 }
 
