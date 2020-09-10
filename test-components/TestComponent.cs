b34601b312f2f0f0fffa0b35488e40cf3745f9f1
JayHales <joshhales2004@gmail.com>
2020-09-10 08:18:01 +0100

    Added a new testing component

---
+++ test-components/TestComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ArmadilloEngine;
+
+public class Test : Component
+{
+    protected override void Start()
+    {
+        Debug.Log("Hello world");
+    }
+
+    protected override void Update()
+    {
+        Debug.Log("hello");
+    }
+}
